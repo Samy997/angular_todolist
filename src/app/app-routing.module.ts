083a39ea3678b9b04dbb641490de3f137e8f8c91
samy997 <zamy.11.4.1997@gmail.com>
2019-04-03 15:17:02 +0200

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Todos Component To Add As A Route
+import { TodosComponent } from './components/todos/todos.component';
+
+import { AboutComponent } from './components/pages/about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
